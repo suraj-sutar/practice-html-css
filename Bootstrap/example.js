1815af1440c53b99ca0df15982a98fb372e20f85
Suraj Tanaji Sutar <surajsutar9922@gmail.com>
2024-02-13 16:12:59 +0530

    bootstrap grid layout
    bootstrap grid layout

---
+++ Bootstrap/example.js
@@ -0,0 +1,31 @@
+// let x = 10;
+// x = "20";
+// console.log(x);
+
+// var a = 20;
+// function updateage() {
+//   var a = 30;
+//   console.log(a);
+// }
+// updateage();
+// console.log(a);
+
+// function Print(name) {
+//   log("doc printed");
+//   return "hi" + name;
+//   console.log(name);
+//   var x = 10;
+//   console.log(x);
+// }
+// Print("angular");
+
+// function addnum(num1, num2) {
+//   // return num1 + "num2";
+//   console.log(num1 + num2);
+//   console.log(typeof addnum);
+// }
+// addnum(10, 20);
+
+class Student{
+    
+}
